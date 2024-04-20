4efccca6fccbcc49d4f46fe28d1979d0987f625c
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-04-20 13:07:53 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/core';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+// Імпортуйте Swiper та модулі навігації та пагінації
+
+// // Імпортуйте CSS-стилі Swiper (це також можна зробити в HTML-файлі)
+// import 'swiper/swiper-bundle.css';
+// import 'swiper/components/navigation/navigation.min.css';
+// import 'swiper/components/pagination/pagination.min.css';
+
+Swiper.use([Navigation, Pagination]);
+
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-  modules: [Navigation, Pagination],
-  // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
+  // Налаштування Swiper
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Додайте інші налаштування за потребою
 });
