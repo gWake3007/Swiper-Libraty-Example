c63fcc0e1bf87f97538d01b09e273ba6c5c743e2
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-04-20 19:09:40 +0200

    1

--- src/main.js
+++ src/main.js
@@ -6,13 +6,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// Імпортуйте Swiper та модулі навігації та пагінації
-
-// // Імпортуйте CSS-стилі Swiper (це також можна зробити в HTML-файлі)
-// import 'swiper/swiper-bundle.css';
-// import 'swiper/components/navigation/navigation.min.css';
-// import 'swiper/components/pagination/pagination.min.css';
-
 Swiper.use([Navigation, Pagination]);
 
 const swiper = new Swiper('.swiper', {
